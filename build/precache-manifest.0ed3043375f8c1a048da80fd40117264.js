e2c3dbdbaca093c6b6cec90f67c8fd99bb4fc40c
daruike <daruike@pinduoduo.com>
2020-06-11 20:21:57 +0800

    v3.0.0

---
+++ build/precache-manifest.0ed3043375f8c1a048da80fd40117264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf9d49d85fe580ee27dd3d7fc49381c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba175c414bb49601781b",
+    "url": "./static/css/main.742fa591.chunk.css"
+  },
+  {
+    "revision": "30fb693f17ae115fed02",
+    "url": "./static/js/2.fa89198f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.fa89198f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba175c414bb49601781b",
+    "url": "./static/js/main.f93589a3.chunk.js"
+  },
+  {
+    "revision": "148150af4644d70f53cb",
+    "url": "./static/js/runtime-main.52b1a8f6.js"
+  }
+]);
